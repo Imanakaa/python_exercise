f74441b230559c189a7b81d9e6bb2c922b71812a
ronekko <ronekko@gmail.com>
2018-03-06 10:32:16 +0900

    Fix "find keywrod"

--- exercise/answer/find_keyword_adv.py
+++ exercise/answer/find_keyword_adv.py
@@ -5,18 +5,22 @@ Created on Fri Mar 13 09:25:17 2015
 @author: sakurai
 """
 
-import google_search_adv
-import urllib2
+import requests
 import re
 
+import google_search_adv
+import reconstruct_text_adv
+
 if __name__ == '__main__':
-    url = google_search_adv.search()
+    text = reconstruct_text_adv.reconstruct()  # さっき作った関数を再利用する
+    url = google_search_adv.search(text)
+    print(url)
+
     # ウェブページのダウンロード
-    f = urllib2.urlopen(url)
-    html = f.read()
-    
+    html = requests.get(url).text
+
     # 正規表現を使う
     pattern = "ディスプレイに対して(.*)を行い、影"
     match = re.search(pattern, html)
     answer = match.group(1)
-    print answer
+    print(answer)
