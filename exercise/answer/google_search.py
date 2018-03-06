3e96137929c206f91ea14cd632315e944928c4d5
ronekko <ronekko@gmail.com>
2018-03-06 10:22:02 +0900

    Fix google search

--- exercise/answer/google_search.py
+++ exercise/answer/google_search.py
@@ -6,8 +6,10 @@ Created on Wed Mar 11 12:08:51 2015
 """
 
 import os
+import urllib
 import natsort
-import google
+import requests
+from bs4 import BeautifulSoup
 
 dir_name = 'data'
 
@@ -26,15 +28,16 @@ for path in file_paths:
         text += char
 # ここまではreconstruct_text.pyと同じ
 
-sentence_end = text.find('。')  # 最初の句点の位置
-sentence = text[:sentence_end] #　0文字目から最初の句点の一文字前までの文字列を切り出す
-query = '"' + sentence + '"'   # ダブルクオーテーションで囲む（Googleで完全一致検索するため）
-results = google.search(query) # 検索結果は一般には複数あることに注意
+sentence_end = text.find('。')   # 最初の句点の位置
+sentence = text[:sentence_end]  # 0文字目から最初の句点の一文字前までの文字列を切り出す
+query = '"' + sentence + '"'    # ダブルクオーテーションで囲む（Googleで完全一致検索するため）
 
-urls = [] # resultsから結果を取り出し、urlsに入れる
-for result in results:
-    urls.append(result)
+# クエリ文字列をGoogle検索し，検索結果画面から該当サイトのURLを抽出する
+res = requests.get('http://google.com/search?q=' + query)
+soup = BeautifulSoup(res.text, 'lxml')
+link_elems = soup.select('.r a')
+url = link_elems[0]['href']
+params = urllib.parse.urlparse(url).query
+url = urllib.parse.parse_qs(params)['q'][0]
 
-print "検索結果:", len(urls), "件"
-url = urls[0]
-print url
+print(url)
